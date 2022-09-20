048872b7595dc0be36276f26e1f9ee287638ad67
Sagar Jha <sagarjha0511@gamil.com>
2022-09-20 16:17:16 +0530

    okkk

---
+++ iterative/fat arrow function/five.js
@@ -0,0 +1,7 @@
+let add = (a, b) => a+b+1;
+function add1(a, b){
+    return a+b+1;
+}
+
+console.log(add(100, 200));
+console.log(add1(100, 200));
---
+++ iterative/fat arrow function/four.js
@@ -0,0 +1,8 @@
+let add =(a,b)=>{
+    return a+b+7
+}
+
+let oggy1 = add(10,20)
+console.log(oggy1)
+let oggy2 = add(1,2)
+console.log(oggy2)
