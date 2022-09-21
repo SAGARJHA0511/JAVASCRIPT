f872aaf04924d5cfb2f8c52d42a01459e390ed1a
Sagar Jha <sagarjha0511@gamil.com>
2022-09-21 23:27:44 +0530

    done

---
+++ iterative/fat arrow function/Object/one.js
@@ -0,0 +1,11 @@
+let e ={
+    id:101,
+    name:'Oggy',
+    salary: 80000
+
+}
+console.log(e);
+console.log(e.id);
+
+// console.log(Object.keys(e)).length
+// console.log(Object.keys(e).length > 0)
